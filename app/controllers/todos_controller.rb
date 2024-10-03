2cade6e1c864670e2fc11d1d28908206df23379d
Rahul Kewalramani <rahool.kewalramani@gmail.com>
2024-10-03 20:03:55 +0530

    Added more feature test

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,11 @@
 class TodosController < ApplicationController
   def index
   end
+
+  def new
+    @todo = Todo.new
+  end
+
+  def create
+  end
 end
---
+++ spec/features/user_create_todos_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature "User Creates Todo" do
+  scenario "successfully" do
+    visit root_path
+    click_on "Add a new Todo"
+
+    fill_in "Title", with: "Buy Milk"
+    click_on "Submit"
+
+    expect(page).to have_css ".todo li", text: "Buy Milk"
+  end
+end
